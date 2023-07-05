07b1bf2645c97e6f0d113318f925767a288eeb4e
Sergey Nilov <s.nilov@tg.dunice.net>
2023-07-05 21:51:40 +0300

    Initial commit

---
+++ src/main/java/com/example/advanced_server/AdvancedServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.advanced_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedServerApplication.class, args);
+	}
+
+}

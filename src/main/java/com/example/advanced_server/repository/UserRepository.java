86e88c08ded998705a81cb0c6b8d67398d44d93b
Sergey Nilov <s.nilov@tg.dunice.net>
2023-07-07 16:01:10 +0300

    fix delete @Repository in feature/#1-register

--- src/main/java/com/example/advanced_server/repository/UserRepository.java
+++ src/main/java/com/example/advanced_server/repository/UserRepository.java
@@ -8,9 +8,7 @@ import com.example.advanced_server.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
     @Query("SELECT u FROM UserEntity u WHERE u.email = :email")
